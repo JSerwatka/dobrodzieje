8493ecc70a7e182c83d2fad2f37193827eff36e8
Jakub Serwatka <kubaserw@gmail.com>
2021-07-28 13:49:42 +0200

    feat: add abstract user model with admin and tests

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+@admin.register(User)
+class UserAdminConfig(UserAdmin):
+    model = User
+    search_fields = ('email',)
+    ordering = ('-date_joined',)
+    list_filter = ('email', 'is_active', 'is_staff')
+    list_display = ('email', 'is_active', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('email',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')})
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_active', 'is_staff')}
+        ),
+    )
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import CustomAccountManager
 
-# Create your models here.
+
+class User(AbstractBaseUser,PermissionsMixin):
+    objects = CustomAccountManager()
+
+    email = models.EmailField(unique=True, blank=False)
+    is_organization = models.BooleanField('is organization?', default=False)
+    is_creator = models.BooleanField('is creator?', default=False)
+    is_staff = models.BooleanField('staff status', default=False)
+    is_active = models.BooleanField('active', default=True)
+    date_joined = models.DateTimeField('date joined', auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+# class Organization(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     user.is_organization = True
+#     name = models.CharField(unique=True, required=True)
+#     fb_url = models.URLField(blank=True, null=True)
+#     twitter_url = models.URLField(blank=True, null=True)
+#     KRS = models.CharField(max_length=10, blank=True, null=True)
+
+# class Creator(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     user.is_creator = True
+#     first_name = models.CharField(max_length=150, blank=True, null=True)
+#     last_name = models.CharField(max_length=150, blank=True, null=True)
