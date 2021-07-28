ae748ab9af24d95d098ae7bb998b501077d3a758
Jakub Serwatka <kubaserw@gmail.com>
2021-07-28 11:36:17 +0200

    refactor: switch db to postgres

--- dobrodzieje/settings/base.py
+++ dobrodzieje/settings/base.py
@@ -16,8 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-#export DJANGO_SETTINGS_MODULE=dobrodzieje.settings.dev
-
 DEBUG = False
 
 # Application definition
--- dobrodzieje/settings/dev.py
+++ dobrodzieje/settings/dev.py
@@ -6,6 +6,9 @@ from dobrodzieje.settings.base import *
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+# export DJANGO_SETTINGS_MODULE=dobrodzieje.settings.dev
+
 SECRET_KEY = 'django-insecure-^a!=*niyi*mcg#1j@az#k2txrv!@m_e+gho_&2tl0&=$zw3!k3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -17,9 +20,13 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'dev', 
+        'USER': 'dev',
+        'PASSWORD': 'dev',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
