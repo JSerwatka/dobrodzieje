a1c84443c3ec35a4050c34bae12923970f8adf67
Jakub Serwatka <kubaserw@gmail.com>
2021-07-29 13:55:44 +0200

    feat: add navbar active class to currently used page

--- webapp/views.py
+++ webapp/views.py
@@ -5,10 +5,26 @@ from django.views.generic import TemplateView
 class Index(TemplateView):
     template_name = 'webapp/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['navbar_active'] = 'index'
+        return context
+
+
 
 class ForCreators(TemplateView):
     template_name = 'webapp/for_creators.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['navbar_active'] = 'for-creators'
+        return context
+
 
 class ForOrganizations(TemplateView):
     template_name = 'webapp/for_organizations.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['navbar_active'] = 'for-organizations'
+        return context
