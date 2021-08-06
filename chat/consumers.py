82c2858483224a2f576b00c288f0e395ae44235d
Jakub Serwatka <kubaserw@gmail.com>
2021-08-06 16:57:03 +0200

    refactor: remove room name from chat urls & routing

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,8 +4,8 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 class ChatRoomConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         self.user = self.scope['user']
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'chat_{self.room_name}'
+        # self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'basic_chat'
         
         await self.channel_layer.group_add(
             self.room_group_name,
