9ff2bf3f7eb3b607c90e7172cffe4282caeb2fb1
Jakub Serwatka <kubaserw@gmail.com>
2021-08-06 21:05:03 +0200

    feat: team_chat handle incorrect requests

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.urls.base import reverse_lazy
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.http import Http404
+
+from webapp.models import Team, Creator
 
 # Create your views here.
 def team_chat(request, team_id):
+    # Check if the team exists and the current user is authorized to view its chat
+    try:
+        Team.objects.get(id=team_id).members.get(user=request.user)
+    except Team.DoesNotExist as e:
+        raise Http404('Drużyna nie istnieje')
+    except Creator.DoesNotExist:
+        messages.error(
+            request, 
+            message='Nie masz uprawnień do tego czatu. Aby z niego skorzystać, poproś o dołączenie.',
+            extra_tags='alert-danger'
+        )
+        return redirect(reverse_lazy('webapp:index'))
+        # raise
+
     return render(request, 'chat/chatroom.html', {
         'team_id': team_id
     })
