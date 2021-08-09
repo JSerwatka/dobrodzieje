ca27966703ad8970dec4712870f832ea9ed8f1bb
Jakub Serwatka <kubaserw@gmail.com>
2021-08-09 14:07:27 +0200

    feat: add host origin validator to chat router

--- dobrodzieje/routing.py
+++ dobrodzieje/routing.py
@@ -1,11 +1,14 @@
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
         )
     ),
 })
