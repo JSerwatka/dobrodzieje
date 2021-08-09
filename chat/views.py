4301d6382917f3e5fb86d7550e67da27510e9922
Jakub Serwatka <kubaserw@gmail.com>
2021-08-09 17:55:43 +0200

    feat: load all msgs in chat view

--- chat/views.py
+++ chat/views.py
@@ -4,10 +4,12 @@ from django.contrib import messages
 from django.http import Http404
 
 from webapp.models import Announcement, Team, Creator
+from chat.models import Message
 
 # Create your views here.
 def team_chat(request, team_id):
     # Check if the team exists and the current user is authorized to view its chat
+    #TODO make sure it works for unauthenticated users
     try:
         team = Team.objects.get(id=team_id)
         team.members.get(user=request.user)
@@ -23,11 +25,14 @@ def team_chat(request, team_id):
     
     members = team.teammember_set.all()
     organization = team.announcement.organization
+    messages = Message.objects.filter(team_id=team_id)
     #TODO settings - delete group, open/close, add looking_for, add stack
 
     return render(request, 'chat/chatroom.html', {
         'team_id': team_id,
         'team': team,
         'members': members,
-        'organization': organization
+        'organization': organization,
+        'messages': messages
     })
+
