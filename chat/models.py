35bc14bb9b3064231f0c241bfd841b7a5ae6f588
Jakub Serwatka <kubaserw@gmail.com>
2021-08-09 12:26:19 +0200

    feat: change message sender to User model

--- chat/models.py
+++ chat/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from webapp.models import Team, Creator
+from webapp.models import Team, User
 
 # Create your models here.
 #TODO Message 
 class Message(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    sender = models.ForeignKey(Creator, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=False, null=False, verbose_name='message content')
     created_on = models.DateTimeField('created on', auto_now_add=True)
 
