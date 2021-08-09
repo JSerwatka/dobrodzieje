30397aae92f7e091997cf5b1b08f4849bb595392
Jakub Serwatka <kubaserw@gmail.com>
2021-08-09 12:30:36 +0200

    feat: add saving messages in chat consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,9 +1,18 @@
 import json
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+from .models import Message
+from webapp.models import Team
+
+@database_sync_to_async
+def save_text_message(message, sender, team_id):
+    Message.objects.create(content=message, sender=sender, team_id=team_id)
+
 
 class ChatRoomConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
-        # self.user = self.scope['user']
+        self.user = self.scope['user']
         # print(self.scope['url_route']['kwargs'])
         self.team_id = self.scope['url_route']['kwargs']['team_id']
         self.room_group_name = f'chat_team_{self.team_id}'
@@ -24,6 +33,7 @@ class ChatRoomConsumer(AsyncJsonWebsocketConsumer):
 
     # Receive from websocket
     async def receive_json(self, text_data):
+        await save_text_message(text_data['message'], self.user, self.team_id)
         # Send to room group
         await self.channel_layer.group_send(
             self.room_group_name,
