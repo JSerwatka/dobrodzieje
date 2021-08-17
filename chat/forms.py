f5e4c46d4e9ab3af3f1d0c1e4313cebf80f40373
Jakub Serwatka <kubaserw@gmail.com>
2021-08-17 13:50:30 +0200

    feat: add stack, looking_for and is_closed filters

--- chat/forms.py
+++ chat/forms.py
@@ -1,10 +1,13 @@
 from django import forms
+from django.forms import widgets
 from webapp.models import Team
 from webapp.models import Roles
 
 class TeamForm(forms.ModelForm):
     our_stack = forms.MultipleChoiceField(label="Jakich technologii używamy", choices=Team.TECHNOLOGIES_CHOICES, required=False)
+    # our_stack = forms.MultipleChoiceField(label="Jakich technologii używamy", choices=Team.TECHNOLOGIES_CHOICES, required=False, widget=forms.Select(attrs={'data-multi-select-plugin': None}))
     looking_for = forms.MultipleChoiceField(label="Kogo poszukujemy:", choices=Roles.choices, required=False)
+    # looking_for = forms.MultipleChoiceField(label="Kogo poszukujemy:", choices=Roles.choices, required=False, widget=forms.Select(attrs={'data-multi-select-plugin': None}))
     is_closed = forms.BooleanField(label='Zamknięta grupa?', required=False)
 
     class Meta:
--- webapp/filters.py
+++ webapp/filters.py
@@ -1,23 +1,36 @@
 from django.contrib.postgres import fields
 from django.db.models.enums import Choices
+from django.forms import widgets
 import django_filters
+from django import forms
 from .models import Announcement, Organization, Team, Roles, City
 
 class AnnouncementFilter(django_filters.FilterSet):
     category = django_filters.ChoiceFilter(label='Wybierz kategorię',
                                            field_name='organization__category', 
                                            choices=Organization.ORGANIZATION_CATEGORY_CHOICES)
-    #TODO show only cities that are used in organizations                                    
-    city = django_filters.ModelChoiceFilter(label='Miasto', field_name='organization__city', queryset=City.objects.used_cities())
+    city = django_filters.ModelChoiceFilter(label='Miasto', 
+                                            field_name='organization__city', 
+                                            queryset=City.objects.used_cities())
+    stack = django_filters.MultipleChoiceFilter(label='W jakich technologiach chcesz pracować',
+                                                field_name='team__our_stack',
+                                                lookup_expr='icontains',
+                                                choices=Team.TECHNOLOGIES_CHOICES)
+    looking_for = django_filters.MultipleChoiceFilter(label='W jakich roli chcesz pracować',
+                                                      field_name='team__looking_for',
+                                                      lookup_expr='icontains',
+                                                      choices=Roles.choices)
     # stack = django_filters.MultipleChoiceFilter(label='W jakich technologiach chcesz pracować',
-    #                                     field_name='announcement__team__our_stack',
-    #                                     choices=Team.TECHNOLOGIES_CHOICES)
+    #                                             field_name='team__our_stack',
+    #                                             choices=Team.TECHNOLOGIES_CHOICES,
+    #                                             widget=forms.Select(attrs={'data-multi-select-plugin': None}))
     # looking_for = django_filters.MultipleChoiceFilter(label='W jakich roli chcesz pracować',
-    #                                     field_name='team__looking_for',
-    #                                     choices=Roles.choices)
-    # is_closed = django_filters.BooleanFilter(label='Czy grupa jest zamknięta',
-    #                                     field_name='team__is_closed')
-
+    #                                                   field_name='team__looking_for',
+    #                                                   choices=Roles.choices,
+    #                                                   widget=forms.Select(attrs={'data-multi-select-plugin': None}))
+    is_closed = django_filters.BooleanFilter(label='Czy grupa jest zamknięta',
+                                             field_name='team__is_closed')
+    #TODO 
     class Meta:
         model = Announcement
         fields = ['category']   
