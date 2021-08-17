c9b875e5f455557d7cf04dfa858a0abc8c7fd6c7
Jakub Serwatka <kubaserw@gmail.com>
2021-08-17 14:34:28 +0200

    feat: add django-debug-toolbar

--- dobrodzieje/urls.py
+++ dobrodzieje/urls.py
@@ -17,14 +17,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import debug_toolbar
 
-app_name = "webapp"
+
+# app_name = "webapp"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('webapp.urls')),
     path('chat/', include('chat.urls')),
-    path('', include('webapp.urls'))
+    path('notifications/', include('notifications.urls')),
 ]
 
 if settings.DEBUG:
+    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
