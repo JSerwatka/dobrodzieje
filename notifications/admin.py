5a263a9a8490a05d5f6c3c49ff67739cbde15f3f
Jakub Serwatka <kubaserw@gmail.com>
2021-08-24 09:54:00 +0200

    feat: add notification model to admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sender', 'message', 'receiver', 'notification_type', 'related_url', 'created_on')
