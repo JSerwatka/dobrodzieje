9fda2e5aa62acbe8cc9b858ed26b656955634354
Jakub Serwatka <kubaserw@gmail.com>
2021-08-24 10:55:53 +0200

    feat: add join announcement request handler

--- notifications/views.py
+++ notifications/views.py
@@ -3,15 +3,23 @@ from webapp.models import (
     User
 )
 
+from .models import Notification
+from django.views.generic import View
+
 # Create your views here.
 def join_announcement(request):
     #TODO add try except for icorrect data send by user
     if request.method == 'POST':
-        actor = User.objects.get(id=request.user.id)
+        sender = request.user
         recipient = User.objects.get(id=request.POST.get('organization'))
-        notification_type = 'Join Announcement'
+        notification_type = Notification.NotificationType.JOIN_REQUEST
         message = 'chce pracować nad Twoją nową stroną'
-        # notify.send(actor, recipient=recipient, verb=notification_type, description=message)
+        Notification.objects.create(
+            sender = sender,
+            recipient = recipient,
+            message = message,
+            notification_type = notification_type
+        )
         return redirect(request.META.get('HTTP_REFERER'))
 
 # def cancel_join_announcement(request):
