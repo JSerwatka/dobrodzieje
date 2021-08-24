8724cc7756b3b4d8d353bbc7d2c3258d2ddccfcd
Jakub Serwatka <kubaserw@gmail.com>
2021-08-24 11:28:52 +0200

    feat: add cancel join announcement logic

--- notifications/urls.py
+++ notifications/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name='notifications'
 urlpatterns = [
     path('join-announcement', views.JoinAnnouncement.as_view(), name='join-announcement'),
+    path('cancel-join-announcement', views.CancelJoinAnnouncement.as_view(), name='cancel-join-announcement'),
 ]
--- notifications/views.py
+++ notifications/views.py
@@ -22,6 +22,11 @@ class JoinAnnouncement(View):
         )
         return redirect(request.META.get('HTTP_REFERER'))
 
-# def cancel_join_announcement(request):
-#     if request.method == 'POST':
-#         print(dir(request))
+class CancelJoinAnnouncement(View):
+    #TODO add try except for icorrect data send by user
+    def post(self, request, *args, **kwargs):
+        sender = request.user
+        recipient = User.objects.get(id=request.POST.get('organization'))
+        notification_type = Notification.NotificationType.JOIN_REQUEST
+        Notification.objects.filter(sender=sender, recipient=recipient, notification_type=notification_type).delete()
+        return redirect(request.META.get('HTTP_REFERER'))
