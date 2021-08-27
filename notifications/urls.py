5d56d632975223a0b232b3b32d9e9e0124d1b59f
Jakub Serwatka <kubaserw@gmail.com>
2021-08-27 13:00:08 +0200

    feat: add join team accept/reject notification to urls

--- notifications/urls.py
+++ notifications/urls.py
@@ -3,10 +3,14 @@ from . import views
 
 app_name='notifications'
 urlpatterns = [
+    #Join announcement
     path('join-announcement', views.JoinAnnouncement.as_view(), name='join-announcement'),
     path('cancel-join-announcement', views.CancelJoinAnnouncement.as_view(), name='cancel-join-announcement'),
-    path('join-announcement-rejection', views.JoinAnnouncementRejection.as_view(), name='join-announcement-rejection'),
     path('join-announcement-acceptance', views.JoinAnnouncementAcceptance.as_view(), name='join-announcement-acceptance'),
+    path('join-announcement-rejection', views.JoinAnnouncementRejection.as_view(), name='join-announcement-rejection'),
+    # Join Team
     path('join-team', views.JoinTeam.as_view(), name='join-team'),
     path('cancel-join-team', views.CancelJoinTeam.as_view(), name='cancel-join-team'),
+    path('join-team-acceptance', views.JoinTeamAcceptance.as_view(), name='join-team-acceptance'),
+    path('join-team-rejection', views.JoinTeamRejection.as_view(), name='join-team-rejection'),
 ]
