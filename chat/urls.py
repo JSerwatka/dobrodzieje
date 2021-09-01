4ba27ed8b35f1c36cb52a5f9ac4fba87f12b34cd
Jakub Serwatka <kubaserw@gmail.com>
2021-09-01 16:42:43 +0200

    feat: add authorization to teamchat view

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name='chat'
 urlpatterns = [
-    path('<int:team_id>', views.team_chat, name='team-chat'),
+    path('<int:team_id>', views.TeamChat.as_view(), name='team-chat'),
     path('<int:team_id>/wiadomosci', views.LoadMessages.as_view(), name='load-messages'),
     path('<int:team_id>/ustawienia', views.UpdateTeamSettings.as_view(), name='update-team-settings'),
     path('<int:team_id>/druzyna', views.DeleteTeam.as_view(), name='delete-team'),
--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,7 @@ from django.urls.base import reverse_lazy
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.http import Http404
+from django.views.generic.base import TemplateView
 from django.views.generic.edit import DeleteView
 from webapp.models import Team, Creator, TeamMember
 from .models import Message
@@ -19,46 +20,83 @@ from .authorization import UserIsTeamMemberTestMixin, UserIsTeamMemberAdminTestM
 
 
 #TODO accept only joined team members
-def team_chat(request, team_id):
-    # Check if the team exists and the current user is authorized to view its chat
-    #TODO make sure it works for unauthenticated users
-    #TODO change to  auhtontication test method
-    try:
-        team = Team.objects.get(id=team_id)
-        team.members.get(user=request.user)
-    except Team.DoesNotExist:
-        raise Http404('Drużyna nie istnieje')
-    except Creator.DoesNotExist:
-        messages.error(
-            request, 
-            message='Nie masz uprawnień do tego czatu. Aby z niego skorzystać, poproś o dołączenie.',
-            extra_tags='alert-danger'
-        )
-        return redirect(reverse_lazy('webapp:index'))
+# def team_chat(request, team_id):
+#     # Check if the team exists and the current user is authorized to view its chat
+#     #TODO make sure it works for unauthenticated users
+#     #TODO change to  auhtontication test method
+#     try:
+#         team = Team.objects.get(id=team_id)
+#         team.members.get(user=request.user)
+#     except Team.DoesNotExist:
+#         raise Http404('Drużyna nie istnieje')
+#     except Creator.DoesNotExist:
+#         messages.error(
+#             request, 
+#             message='Nie masz uprawnień do tego czatu. Aby z niego skorzystać, poproś o dołączenie.',
+#             extra_tags='alert-danger'
+#         )
+#         return redirect(reverse_lazy('webapp:index'))
     
-    members = team.teammember_set.select_related('creator__user')
-    current_member = members.get(creator__user=request.user)
+#     members = team.teammember_set.select_related('creator__user')
+#     current_member = members.get(creator__user=request.user)
 
-    # New members have to enter their nick
-    if not current_member.joined:
-        return redirect(
-                reverse_lazy('chat:join-chat', 
-                kwargs={
-                    'team_id': team_id,
-                    'team_member_id': current_member.id
-                }))
+#     # New members have to enter their nick
+#     if not current_member.joined:
+#         return redirect(
+#                 reverse_lazy('chat:join-chat', 
+#                 kwargs={
+#                     'team_id': team_id,
+#                     'team_member_id': current_member.id
+#                 }))
 
-    current_member_admin = current_member.is_admin
-    organization = team.announcement.organization
+#     current_member_admin = current_member.is_admin
+#     organization = team.announcement.organization
 
-    return render(request, 'chat/chatroom.html', {
-        'team_id': team_id,
-        'team': team,
-        'members': members,
-        'organization': organization,
-        'user_is_admin': current_member_admin,
-        'form': TeamForm(instance=team)
-    })
+#     return render(request, 'chat/chatroom.html', {
+#         'team_id': team_id,
+#         'team': team,
+#         'members': members,
+#         'organization': organization,
+#         'user_is_admin': current_member_admin,
+#         'form': TeamForm(instance=team)
+#     })
+
+class TeamChat(UserIsTeamMemberTestMixin, TemplateView):
+    template_name = 'chat/chatroom.html'
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        team = context['team']
+        current_member = context['current_member']
+
+        # New members have to enter their nick first
+        if not current_member.joined:
+            return redirect(
+                    reverse_lazy('chat:join-chat', 
+                    kwargs={
+                        'team_id': team.id,
+                        'team_member_id': current_member.id
+                    }))
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        team = Team.objects.get(id=kwargs['team_id'])
+        members = team.teammember_set.select_related('creator__user')
+        current_member = members.get(creator__user=self.request.user)
+        organization = team.announcement.organization
+
+        context.update({
+            'team_id': team.id,
+            'team': team,
+            'members': members,
+            'organization': organization,
+            'current_member': current_member,
+            'form': TeamForm(instance=team)
+        })
+
+        return context
 
 
 class JoinChat(UserIsTeamMemberTestMixin, UpdateView):
